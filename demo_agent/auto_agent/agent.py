7a39c1fad20e827a63214d91f70679d89efa7588
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2025-03-27 03:20:17 -0400

    Fix prompt-path

--- demo_agent/auto_agent/agent.py
+++ demo_agent/auto_agent/agent.py
@@ -212,6 +212,9 @@ if __name__ == "__main__":
     parser.add_argument("--node-name", type=str, required=True)
     parser.add_argument("--env-uuid", type=str, required=True)
     parser.add_argument("--redis-url", type=str, default="redis://localhost:6379/0")
+    parser.add_argument(
+        "--prompt-path", type=str, default="demo_agent/auto_agent/prompts.yaml"
+    )
     parser.add_argument("--add-task-demo", action="store_true")
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
